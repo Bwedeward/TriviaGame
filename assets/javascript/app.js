d76894ea2cab9e4ce04458f7484810f82329fa5d
Brooke W <bwedeward@gmail.com>
2020-04-01 17:17:16 -0500

    add js for questions and have appear on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,94 @@
 //on open, title and start button show
 //when start is clicked:
-    //timer starts counting down
-    //questions appear with answer options beneath them
-    //player is able to select options
-    //player can only select one option per question
+//timer starts counting down
+//questions appear with answer options beneath them
+//player is able to select options
+//player can only select one option per question
 //when timer stops screen shows correct/incorrect/unanswered
-//if player finishes questions before timer: 
-    //click "done" button
-    //screen shows correct/incorrect/unanswered
+//if player finishes questions before timer:
+//click "done" button
+//screen shows correct/incorrect/unanswered
 
-$(document).ready(){
+$(document).ready(function() {
+  var correct = 0;
+  var incorrect = 0;
+  var unanswered = 0;
+  var timer = 60;
+  var timerOn = false;
 
-    correct: 0;
-    incorrect: 0;
-    unanswered: 0;
-    timer: 60;
-    timerOn: false;
+  var questions = $(".questions");
 
-    var triviaQuestions = [{
-        question: "What pet did Harry Potter Have?"
-        options: ["Toad", "Owl", "Lizard", "Cat"]
-        answer: "1"
-    }
+  var triviaQuestions = [
     {
-        question: "How did students get to Hogwarts?"
-        options: ["Car", "Brooms", "Train", "Magic Carpets"]
-        answer: "2"
-    }
+      question: "What pet did Harry Potter Have?",
+      options: ["Toad", "Owl", "Lizard", "Cat"],
+      answer: "1"
+    },
     {
-        question: "Who tried to kill Harry?"
-        options: ["Snape", "Hagrid", "Voldemort", "Dudley"]
-        answer: "2"
-    }
+      question: "How did students get to Hogwarts?",
+      options: ["Car", "Brooms", "Train", "Magic Carpets"],
+      answer: "2"
+    },
     {
-        question: "What is the house number Harry grew up in?"
-        options: ["Four", "Twelve", "Six", "Fourteen"]
-        answer: "0"
-    }
+      question: "Who tried to kill Harry?",
+      options: ["Snape", "Hagrid", "Voldemort", "Dudley"],
+      answer: "2"
+    },
     {
-        question: "Who told Harry he was a wizard?"
-        options: ["Dumbledore", "McGonagall", "Snape", "Hagrid"]
-        answer: "3"
-    }
+      question: "What is the house number Harry grew up in?",
+      options: ["Four", "Twelve", "Six", "Fourteen"],
+      answer: "0"
+    },
     {
-        question: "What was Harry's dad's name?"
-        options: ["Joe", "Sirius", "James", "Remus"]
-        answer: "2"
-    }
+      question: "Who told Harry he was a wizard?",
+      options: ["Dumbledore", "McGonagall", "Snape", "Hagrid"],
+      answer: "3"
+    },
     {
-        question: "Who has a half brother that's a giant?"
-        options: ["Hagrid", "Ron", "Harry", "Hermoine"]
-        answer: "0"
-    }{
-        question: "What position does Harry play in Quidditch?"
-        options: ["Beater", "Seeker", "Chaser", "Squabbler"]
-        answer: "1"
-    }]
+      question: "What was Harry's dad's name?",
+      options: ["Joe", "Sirius", "James", "Remus"],
+      answer: "2"
+    },
+    {
+      question: "Who has a half brother that's a giant?",
+      options: ["Hagrid", "Ron", "Harry", "Hermoine"],
+      answer: "0"
+    },
+    {
+      question: "What position does Harry play in Quidditch?",
+      options: ["Beater", "Seeker", "Chaser", "Squabbler"],
+      answer: "1"
+    }
+  ];
 
-    function startTrivia(){
-        
-    }
+  $(".startButton").on("click", function() {
+    console.log("CLICK AND I'M HERE");
+    startTrivia();
+  });
 
-    function countDown (){
-        
-    }
+  function startTrivia() {
+    for (var index = 0; index < triviaQuestions.length; index++) {
+      var question = $("<p>");
+      question.text(triviaQuestions[index].question);
 
-}
+      for (var i = 0; i < triviaQuestions[index].options.length; i++) {
+        var option = $("<label class ='btn btn-secondary'>");
+        var input = $(
+          `<input type='radio' id='option${i + 1}' name='option${i + 1}'>`
+        );
+        option.append(input)
+        option.text(triviaQuestions[index].options[i])
+        question.append(option)
+      }
+      questions.append(question);
+    }
+  }
+
+  // function countDown (){
+
+  // }
+
+  // for (var i = 0; i < triviaQuestions.length; i++){
+
+  // }
+});
